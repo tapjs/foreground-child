d60c88e59f549d476c542de806b507513d1151af
Benjamin Coe <ben@npmjs.com>
2015-06-17 17:54:22 -0400

    beforeExit hook

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var signalExit = require('signal-exit')
 var spawn = require('child_process').spawn
 
-module.exports = function (program, args) {
+module.exports = function (program, args, cb) {
+
+  if (typeof args === 'function') {
+    cb = args
+    args = undefined
+  }
+
+  cb = cb || function (done) {
+    return done()
+  }
+
   if (Array.isArray(program)) {
     args = program.slice(1)
     program = program[0]
@@ -17,17 +27,19 @@ module.exports = function (program, args) {
   })
 
   child.on('close', function (code, signal) {
-    childExited = true
-    if (signal) {
-      // If there is nothing else keeping the event loop alive,
-      // then there's a race between a graceful exit and getting
-      // the signal to this process.  Put this timeout here to
-      // make sure we're still alive to get the signal, and thus
-      // exit with the intended signal code.
-      setTimeout(function () {}, 200)
-      process.kill(process.pid, signal)
-    } else
-      process.exit(code)
+    cb(function () {
+      childExited = true
+      if (signal) {
+        // If there is nothing else keeping the event loop alive,
+        // then there's a race between a graceful exit and getting
+        // the signal to this process.  Put this timeout here to
+        // make sure we're still alive to get the signal, and thus
+        // exit with the intended signal code.
+        setTimeout(function () {}, 200)
+        process.kill(process.pid, signal)
+      } else
+        process.exit(code)      
+    })
   })
 
   return child
