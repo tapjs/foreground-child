98d1854278a120cf057ad5301ee82e41c5809591
Ryan Graham <r.m.graham@gmail.com>
2016-05-28 16:53:41 -0700

    replace cross-spawn-async with cross-spawn
    The logic for checking whether cross-spawn-async needed to be called is
    implemented directly in cross-spawn, so also remove our version of the
    logic and the corresponding direct dependency on 'which'.
    
    Replaces #8

--- index.js
+++ index.js
@@ -1,38 +1,5 @@
 var signalExit = require('signal-exit')
-var spawn = require('child_process').spawn
-var crossSpawn = require('cross-spawn-async')
-var fs = require('fs')
-var which = require('which')
-
-function needsCrossSpawn (exe) {
-  if (process.platform !== 'win32') {
-    return false
-  }
-
-  try {
-    exe = which.sync(exe)
-  } catch (er) {
-    // failure to find the file?  cmd probably needed.
-    return true
-  }
-
-  if (/\.(com|cmd|bat)$/i.test(exe)) {
-    // need cmd.exe to run command and batch files
-    return true
-  }
-
-  var buffer = new Buffer(150)
-  try {
-    var fd = fs.openSync(exe, 'r')
-    fs.readSync(fd, buffer, 0, 150, 0)
-  } catch (e) {
-    // If it's not an actual file, probably it needs cmd.exe.
-    // also, would be unsafe to test arbitrary memory on next line!
-    return true
-  }
-
-  return /\#\!(.+)/i.test(buffer.toString().trim())
-}
+var spawn = require('cross-spawn')
 
 module.exports = function (program, args, cb) {
   var arrayIndex = arguments.length
@@ -60,8 +27,7 @@ module.exports = function (program, args, cb) {
     args = [].slice.call(arguments, 1, arrayIndex)
   }
 
-  var spawnfn = needsCrossSpawn(program) ? crossSpawn : spawn
-  var child = spawnfn(program, args, { stdio: 'inherit' })
+  var child = spawn(program, args, { stdio: 'inherit' })
 
   var childExited = false
   signalExit(function (code, signal) {
